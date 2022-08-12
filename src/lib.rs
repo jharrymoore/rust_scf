b15f9dfee50c4d4bca345814240e539a3f866ba2
Moore, Harry <harry.moore@astrazeneca.com>
2022-08-12 17:46:14 +0200

    bug fixes in integral calculations

--- src/lib.rs
+++ src/lib.rs
@@ -2,31 +2,30 @@
 pub mod integrals {
     use std::f64::consts::PI;
 
+    use ndarray_linalg::Scalar;
     use statrs::function::erf::erf;
     pub fn overlap_integral(a: f64, b: f64, r: f64) -> f64 {
-        PI / (a + b).powf(1.5) * ((-a * b / (a + b)) * r.powf(2.)).exp()
+        (PI / (a + b)).powf(1.5) * ((-a * b / (a + b)) * r.powf(2.)).exp()
     }
 
-    pub fn kinetic_integral(a: f64, b: f64, r: f64) -> f64 {
-        (a * b / (a + b)) * (3. - 2. * a * b / (a + b) * r.powf(2.)) * (PI / (a + b)).powf(1.5)
+    pub fn kinetic_integral(a: f64, b: f64, r_ab: f64) -> f64 {
+        (a * b / (a + b)) * (3. - 2. * a * b / (a + b) * r_ab.powf(2.)) * (PI / (a + b)).powf(1.5) * (-a * b / (a + b) * r_ab.pow(2.)).exp()
     }
 
     pub fn nuclear_attraction_integral(a: f64, b: f64, r_ab: f64, r_pc: f64, z_c: u32) -> f64 {
         // Computes the nuclear attraction integral for <A|-Z/r|B> two basis functions A and B at separation r
-        let val = -2. * PI / (a + b)
+        -2. * PI / (a + b)
             * z_c as f64
             * (-a * b / (a + b) * r_ab.powf(2.)).exp()
-            * f_0((a + b) * r_pc.powf(2.));
-		println!("Nuclear attraction integral: {}\n\n", val);
-		val
+            * f_0((a + b) * r_pc.powf(2.))
     }
 
     pub fn f_0(t: f64) -> f64 {
-		if t < 1e-6 {
-			 1. - t / 3.
-		} else {
-			0.5 * (PI / t).powf(0.5) * erf(t.powf(0.5))
-		}
+        if t < 1e-6 {
+            1. - t / 3.
+        } else {
+            0.5 * (PI / t).powf(0.5) * erf(t.powf(0.5))
+        }
     }
 
     pub fn two_electron_integral(
@@ -38,7 +37,7 @@ pub mod integrals {
         r_kl: f64,
         r_pq: f64,
     ) -> f64 {
-        (2. * PI).powf(2.5) / ((a + b) * (c * d) * (a + b + c + d).powf(0.5))
+        2. * PI.powf(2.5) / ((a + b) * (c * d) * (a + b + c + d).sqrt())
             * (-a * b / (a + b) * r_ij.powf(2.) - c * d / (c + d) * r_kl.powf(2.)).exp()
             * f_0((a + b) * (c + d) / (a + b + c + d) * r_pq)
     }
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,9 @@
 use clap::Parser;
 use ndarray::{Array, Array2, Array4};
-use ndarray_linalg::{UPLO, Eigh};
-use rust_scf::integrals::{kinetic_integral, nuclear_attraction_integral, overlap_integral, two_electron_integral};
+use ndarray_linalg::{Eigh, UPLO};
+use rust_scf::integrals::{
+    kinetic_integral, nuclear_attraction_integral, overlap_integral, two_electron_integral,
+};
 use std::f64::consts::PI;
 
 #[derive(Parser, Debug)]
@@ -70,24 +72,20 @@ fn hf_calc(n: u32, r: f64, zeta_1: f64, zeta_2: f64, z_a: u32, z_b: u32) {
 
     // basis functions on atom 1
     let mut a1 = expons.clone();
-    a1 = a1.iter_mut().map(|x| *x * zeta_1.powf(2.)).collect();
+    a1.iter_mut().for_each(|x| *x *= zeta_1.powf(2.));
     let mut d1 = coeffs.clone();
-    d1 = d1
+    d1
         .indexed_iter_mut()
-        .map(|(idx, x)| *x * &a1[idx] * (2. / PI).powf(0.75))
-        .collect();
+        .for_each(|(idx, x)| *x *= (&a1[idx] * 2. / PI).powf(0.75));
 
     // basis functions on atom 2
     let mut a2 = expons.clone();
-    a2 = a2.iter_mut().map(|x| *x * zeta_1.powf(2.)).collect();
+    a2.iter_mut().for_each(|x| *x *= zeta_2.powf(2.));
     let mut d2 = coeffs.clone();
-    d2 = d2
+    d2
         .indexed_iter_mut()
-        .map(|(idx, x)| *x * &a2[idx] * (2. / PI).powf(0.75))
-        .collect();
+        .for_each(|(idx, x)| *x *= (&a2[idx] * 2. / PI).powf(0.75));
     println!("{:?}", d2);
-
-    // let all_coeffs = Array::from_shape_vec((3,1), )
 
     // now we have the normalized basis functions.  Compute the standard integrals
     /* Integrals are the following
@@ -112,213 +110,261 @@ fn hf_calc(n: u32, r: f64, zeta_1: f64, zeta_2: f64, z_a: u32, z_b: u32) {
     let mut v12a = 0.;
     let mut v12b = 0.;
 
-    // loop over the number of electrons
-    for i  in 0..n {
+    // loop over the basis functions
+    for i in 0..n {
         for j in 0..n {
-			let i: usize = i.try_into().unwrap();
-			let j: usize = j.try_into().unwrap();
-			let r_ap = a2[j] * r / (a1[i] + a2[j]);
-			let r_bp = r - r_ap;
-            s12 += overlap_integral(a1[i], a2[j], r) * d1[i] * d2[j];
-            t11 += kinetic_integral(a1[i], a1[j], 0.) * d1[i] * d1[j];
-            t12 += kinetic_integral(a1[i], a2[j], r) * d1[i] * d2[j];
-            t22 += kinetic_integral(a1[i], a2[j], 0.) * d2[i] * d2[j];
+            let i: usize = i.try_into().unwrap();
+            let j: usize = j.try_into().unwrap();
+            let r_ap = a2[j] * r / (a1[i] + a2[j]);
+            let r_bp = r - r_ap;
+            s12 += overlap_integral(a1[i], a2[j], r) * d1[i] * d2[j]; /*not working */
+            t11 += kinetic_integral(a1[i], a1[j], 0.) * d1[i] * d1[j]; /* working */
+            t12 += kinetic_integral(a1[i], a2[j], r) * d1[i] * d2[j]; /* not working */
+            t22 += kinetic_integral(a2[i], a2[j], 0.) * d2[i] * d2[j]; /* working */
             v11a += nuclear_attraction_integral(a1[i], a1[j], 0., 0., z_a) * d1[i] * d1[j];
-            v11b += nuclear_attraction_integral(a1[i], a1[j], 0., 0., z_b)* d1[i] * d1[j];
-            v22a += nuclear_attraction_integral(a2[i], a2[j], 0., 0., z_a)* d2[i] * d2[j];
-            v22b += nuclear_attraction_integral(a2[i], a2[j], 0., 0., z_b)* d2[i] * d2[j];
-            v12a += nuclear_attraction_integral(a1[i], a2[j], r, r_ap, z_a)* d1[i] * d2[j];
-            v12b += nuclear_attraction_integral(a1[i], a2[j], r, r_bp, z_b)* d1[i] * d2[j];
-
-           
+            v11b += nuclear_attraction_integral(a1[i], a1[j], 0., r, z_b) * d1[i] * d1[j];
+            v22a += nuclear_attraction_integral(a2[i], a2[j], 0., r, z_a) * d2[i] * d2[j];
+            v22b += nuclear_attraction_integral(a2[i], a2[j], 0., 0., z_b) * d2[i] * d2[j];
+            v12a += nuclear_attraction_integral(a1[i], a2[j], r, r_ap, z_a) * d1[i] * d2[j];
+            v12b += nuclear_attraction_integral(a1[i], a2[j], r, r_bp, z_b) * d1[i] * d2[j];
         }
     }
-	let mut v1111 = 0.;
-	let mut v2111 = 0.;
-	let mut v2121 = 0.;
-	let mut v2211 = 0.;
-	let mut v2221 = 0.;
-	let mut v2222 = 0.;
+	println!("s12: {} \tt11: {}\tt12: {}\tt22: {}\tv11a: {}\tv11b: {}\tv12a: {}\tv12b: {}", s12, t11, t12, t22, v11a, v11b, v12a, v12b);
+    let mut v1111 = 0.;
+    let mut v2111 = 0.;
+    let mut v2121 = 0.;
+    let mut v2211 = 0.;
+    let mut v2221 = 0.;
+    let mut v2222 = 0.;
 
-	for i in 0..n {
-		for j in 0..n {
-			for k in 0..n {
-				for l in 0..n {
-					let i: usize = i.try_into().unwrap();
-					let j: usize = j.try_into().unwrap();
-					let k: usize = k.try_into().unwrap();
-					let l: usize = l.try_into().unwrap();
-					let r_ap = a2[j] * r / (a2[i] + a1[j]);
-					let r_bp = r - r_ap;
-					let r_aq = a2[k] * r / (a2[k] + a1[l]);
-					let r_bq = r - r_aq;
-					let r_pq  = r_ap - r_aq;
-					v1111 += two_electron_integral(a1[i], a1[j], a1[k], a1[l], 0., 0., 0.) * d1[i] * d1[j] * d1[k] * d1[l];
-					v2111 += two_electron_integral(a2[i], a1[j], a1[k], a1[l], r, 0., r_ap) * d2[i] * d1[j] * d1[k] * d1[l];
-					v2121 += two_electron_integral(a2[i], a1[j], a2[k], a1[l], r, r, r_pq) * d2[i] * d1[j] * d2[k] * d1[l];
-					v2211 += two_electron_integral(a2[i], a2[j], a1[k], a1[l], 0., 0., r) * d2[i] * d2[j] * d1[k] * d1[l];
-					v2221 += two_electron_integral(a2[i], a2[j], a2[k], a1[l], 0., r, r_bq) * d2[i] * d2[j] * d2[k] * d1[l];
-					v2222 += two_electron_integral(a2[i], a2[j], a2[k], a2[l], 0., 0., 0.) * d2[i] * d2[j] * d2[k] * d2[l];
+    for i in 0..n {
+        for j in 0..n {
+            for k in 0..n {
+                for l in 0..n {
+                    let i: usize = i.try_into().unwrap();
+                    let j: usize = j.try_into().unwrap();
+                    let k: usize = k.try_into().unwrap();
+                    let l: usize = l.try_into().unwrap();
+                    let r_ap = a2[i] * r / (a2[i] + a1[j]);
+                    let r_bp = r - r_ap;
+                    let r_aq = a2[k] * r / (a2[k] + a1[l]);
+                    let r_bq = r - r_aq;
+                    let r_pq = r_ap - r_aq;
+                    v1111 += two_electron_integral(a1[i], a1[j], a1[k], a1[l], 0., 0., 0.)
+                        * d1[i]
+                        * d1[j]
+                        * d1[k]
+                        * d1[l];
+                    v2111 += two_electron_integral(a2[i], a1[j], a1[k], a1[l], r, 0., r_ap)
+                        * d2[i]
+                        * d1[j]
+                        * d1[k]
+                        * d1[l];
+                    v2121 += two_electron_integral(a2[i], a1[j], a2[k], a1[l], r, r, r_pq)
+                        * d2[i]
+                        * d1[j]
+                        * d2[k]
+                        * d1[l];
+                    v2211 += two_electron_integral(a2[i], a2[j], a1[k], a1[l], 0., 0., r)
+                        * d2[i]
+                        * d2[j]
+                        * d1[k]
+                        * d1[l];
+                    v2221 += two_electron_integral(a2[i], a2[j], a2[k], a1[l], 0., r, r_bq)
+                        * d2[i]
+                        * d2[j]
+                        * d2[k]
+                        * d1[l];
+                    v2222 += two_electron_integral(a2[i], a2[j], a2[k], a2[l], 0., 0., 0.)
+                        * d2[i]
+                        * d2[j]
+                        * d2[k]
+                        * d2[l];
+                }
+            }
+        }
+    }
 
+	println!("v1111: {} \tv2211: {}", v1111, v2211);
+    let (h, s, x, xt, tt) = collect(
+        s12, t11, t12, t22, v11a, v11b, v22a, v22b, v12a, v12b, v1111, v2111, v2121, v2211, v2221,
+        v2222,
+    );
 
-				}
-			}
-		}
-	}
-	
-	let (h, s, x, xt, tt) = collect(s12, t11, t12, t22, v11a, v11b, v22a, v22b, v12a, v12b, v1111, v2111, v2121, v2211, v2221, v2222);
-
-
-	scf(r, z_a, z_b, h, s, x, xt, tt);
-    
+    scf(r, z_a, z_b, h, s, x, xt, tt);
 }
 
-fn collect(s12: f64, t11: f64, t12: f64, t22: f64, v11a: f64, v11b: f64, v22a: f64, v22b: f64, v12a: f64, v12b: f64, v1111: f64, v2111: f64, v2121: f64, v2211: f64, v2221: f64, v2222: f64) -> (Array2<f64>, Array2<f64>, Array2<f64>, Array2<f64>, Array4<f64>){
-	// put the values into the necessary arrays
-	let mut h = Array::zeros((2,2));
-	let mut s = Array::eye(2);
-	let mut x = Array::zeros((2,2));
-	// store two-electron integrals
-	let mut t = Array::zeros((2,2,2,2));
-	println!("{},{},{}", t11, v11a, v11b);
-	h[[0,0]] = t11 + v11a + v11b;
-	h[[0,1]] = t12 + v12a + v12b;
-	h[[1,0]] = h[[0,1]];
-	h[[1,1]] = t22 + v22a + v22b;
+fn collect(
+    s12: f64,
+    t11: f64,
+    t12: f64,
+    t22: f64,
+    v11a: f64,
+    v11b: f64,
+    v22a: f64,
+    v22b: f64,
+    v12a: f64,
+    v12b: f64,
+    v1111: f64,
+    v2111: f64,
+    v2121: f64,
+    v2211: f64,
+    v2221: f64,
+    v2222: f64,
+) -> (
+    Array2<f64>,
+    Array2<f64>,
+    Array2<f64>,
+    Array2<f64>,
+    Array4<f64>,
+) {
+    // put the values into the necessary arrays
+    let mut h = Array::zeros((2, 2));
+    let mut s = Array::eye(2);
+    let mut x = Array::zeros((2, 2));
+    // store two-electron integrals
+    let mut t = Array::zeros((2, 2, 2, 2));
+    h[[0, 0]] = t11 + v11a + v11b;
+    h[[0, 1]] = t12 + v12a + v12b;
+    h[[1, 0]] = h[[0, 1]];
+    h[[1, 1]] = t22 + v22a + v22b;
 
-	s[[0,1]] = s12;
-	s[[1,0]] = s12;
+    s[[0, 1]] = s12;
+    s[[1, 0]] = s12;
 
-	t[[0,0,0,0]] = v1111;
+    t[[0, 0, 0, 0]] = v1111;
 
-	t[[1,0,0,0]] = v2111;
-	t[[0,1,0,0]] = v2111;
-	t[[0,0,1,0]] = v2111;
-	t[[0,0,0,1]] = v2111;
-	
-	t[[1,0,1,0]] = v2121;
-	t[[0,1,1,0]] = v2121;
-	t[[1,0,0,1]] = v2121;
-	t[[0,1,0,1]] = v2121;
+    t[[1, 0, 0, 0]] = v2111;
+    t[[0, 1, 0, 0]] = v2111;
+    t[[0, 0, 1, 0]] = v2111;
+    t[[0, 0, 0, 1]] = v2111;
 
-	t[[1,1,0,0]] = v2211;
-	t[[0,0,1,1]] = v2211;
+    t[[1, 0, 1, 0]] = v2121;
+    t[[0, 1, 1, 0]] = v2121;
+    t[[1, 0, 0, 1]] = v2121;
+    t[[0, 1, 0, 1]] = v2121;
 
-	t[[1,1,1,0]] = v2221;
-	t[[1,1,0,1]] = v2221;
-	t[[1,0,1,1]] = v2221;
-	t[[0,1,1,1]] = v2221;
+    t[[1, 1, 0, 0]] = v2211;
+    t[[0, 0, 1, 1]] = v2211;
 
-	t[[1,1,1,1]] = v2222;
+    t[[1, 1, 1, 0]] = v2221;
+    t[[1, 1, 0, 1]] = v2221;
+    t[[1, 0, 1, 1]] = v2221;
+    t[[0, 1, 1, 1]] = v2221;
 
+    t[[1, 1, 1, 1]] = v2222;
 
-	// canonical normalization
-	x[[0,0]] = 1. / (2. * (1. + s12)).sqrt();
-	x[[1,0]] = x[[0,0]];
-	x[[0,1]] = 1. / (2. * (1. - s12)).sqrt();
-	x[[1,1]] = -x[[0,1]];
+    // canonical normalization
+    x[[0, 0]] = 1. / (2. * (1. + s12)).sqrt();
+    x[[1, 0]] = x[[0, 0]];
+    x[[0, 1]] = 1. / (2. * (1. - s12)).sqrt();
+    x[[1, 1]] = -x[[0, 1]];
 
-	let xt = x.clone().reversed_axes();
+    let xt = x.clone().reversed_axes();
 
-	println!("h: {:?}\ns: {:?}\nt: {:?}\n\n", h, s, t);
+    println!("h: {:?}\ns: {:?}\nt: {:?}\n\n", h, s, t);
 
-	return (h, s, x, xt, t)
-
+    return (h, s, x, xt, t);
 }
 
 fn compute_g_matrix(p: &Array2<f64>, tt: &Array4<f64>) -> Array2<f64> {
-	let mut g = Array::zeros((2,2));
-	for i in 0..2 {
-		for j in 0..2 {
-			for k in 0..2 {
-				for l in 0..2 {
-					// coulomb - exchange * P matrix
-					g[[i,j]] = p[[k,l]] * (tt[[i,j,k,l]] - 0.5 * tt[[i,j,k,l]])
-				}
-			}
-		}
-	}
-	g
+    let mut g = Array::zeros((2, 2));
+    for i in 0..2 {
+        for j in 0..2 {
+            for k in 0..2 {
+                for l in 0..2 {
+                    // coulomb - exchange * P matrix
+                    g[[i, j]] = p[[k, l]] * (tt[[i, j, k, l]] - 0.5 * tt[[i, j, k, l]])
+                }
+            }
+        }
+    }
+    g
 }
 
-fn scf(r: f64, z_a: u32, z_b: u32, h: Array2<f64>, s: Array2<f64>, x: Array2<f64>, xt: Array2<f64>, tt: Array4<f64>) {
+fn scf(
+    r: f64,
+    z_a: u32,
+    z_b: u32,
+    h: Array2<f64>,
+    s: Array2<f64>,
+    x: Array2<f64>,
+    xt: Array2<f64>,
+    tt: Array4<f64>,
+) {
+    let criteria = 1e-3;
+    let maxiter = 50;
+    let mut iter = 0;
 
-	let criteria = 1e-3;
-	let maxiter = 50;
-	let mut iter = 0;
+    // initial guess at the density matrix, i.e. use core hamiltonian term only
+    let mut p = Array::zeros((2, 2));
+    let mut f = h.clone();
+    let mut e_elec = 0.;
+    while iter < maxiter {
+        iter += 1;
+        // generate the G matrix from P and the two electron integrals
+        let mut g = compute_g_matrix(&p, &tt);
 
+        // generate fock matrix
 
-	// initial guess at the density matrix, i.e. use core hamiltonian term only
-	let mut p = Array::zeros((2,2));
-	let mut f = h.clone();
-	let mut e_elec = 0.;
-	while iter < maxiter {
-		iter += 1;
-		// generate the G matrix from P and the two electron integrals
-		let mut g = compute_g_matrix(&p, &tt);
+        f.indexed_iter_mut().for_each(|((i, j), x)| *x += g[[i, j]]);
 
-		// generate fock matrix 
-		
-		f.indexed_iter_mut().for_each(|((i,j), x)| *x += g[[i,j]]);
-		
+        // compute electronic energy = $E_0 = 0.5 * \Sum_{u,v} P_{\mu,\nu} (H_{\mu, \nu} + F_{\mu,\nu}$
+        e_elec = 0.;
+        for i in 0..2 {
+            for j in 0..2 {
+                e_elec += 0.5 * p[[i, j]] * (h[[i, j]] + f[[i, j]]);
+            }
+        }
+        println!("Iteration: {}, \nEnergy: {}\n\n", &iter, &e_elec);
 
-		// compute electronic energy = $E_0 = 0.5 * \Sum_{u,v} P_{\mu,\nu} (H_{\mu, \nu} + F_{\mu,\nu}$ 
-		e_elec = 0.;
-		for i in 0..2 {
-			for j in 0..2 {
-				e_elec += 0.5 * p[[i,j]] * (h[[i,j]] + f[[i,j]]);
-			}
-		}
-		println!("Iteration: {}, \nEnergy: {}\n\n", &iter, &e_elec);
+        // Transform the fock matrix
+        g = f.clone().dot(&x);
+        let f_prime = xt.clone().dot(&g);
 
-		// Transform the fock matrix 
-		g = f.clone().dot(&x);
-		let f_prime = xt.clone().dot(&g);
+        // eigenvalues e (orbital energies), eigenvectors c_prime (transformed coefficient matrix)
+        let (e, c_prime) = f_prime.eigh(UPLO::Lower).unwrap();
+        // transform eigenvectors
+        let c = x.clone().dot(&c_prime);
 
-		// eigenvalues e (orbital energies), eigenvectors c_prime (transformed coefficient matrix)
-		let (e, c_prime) = f_prime.eigh(UPLO::Lower).unwrap();
-		// transform eigenvectors 
-		let c = x.clone().dot(&c_prime);
+        // compute new density matrix P
+        let p_prev = p.clone();
+        // eqn 3.145
+        p = Array::zeros((2, 2));
+        for i in 0..2 {
+            for j in 0..2 {
+                // use zero index since only one occupied orbital here
+                p[[i, j]] += 2. * c[[i, 0]] * c[[j, 0]]
+            }
+        }
+        // println!("new density matrix{:?}", p);
 
-		// compute new density matrix P
-		let p_prev = p.clone();
-		// eqn 3.145
-		p = Array::zeros((2,2));
-		for i in 0..2 {
-			for j in 0..2 {
-				// use zero index since only one occupied orbital here
-				p[[i,j]] += 2. * c[[i,0]] * c[[j,0]]
-			}
-		}
+        // compute delta in P matrix
+        let mut delta = 0.;
+        for i in 0..2 {
+            for j in 0..2 {
+                delta += (p[[i, j]] - p_prev[[i, j]]).powf(2.)
+            }
+        }
+        delta = (delta / 4.).sqrt();
 
-		// compute delta in P matrix
-		let mut  delta = 0.;
-		for i in 0..2 {
-			for j in 0..2 {
-				delta += (p[[i,j]] - p_prev[[i,j]]).powf(2.)
-			}
-		}
-		delta = (delta / 4.).sqrt();
+        println!("Iter: {}, \nDelta: {}", iter, delta);
+        if delta < criteria || iter > maxiter {
+            println!("SCF finished!");
+            break;
+        }
+    }
 
-		println!("Iter: {}, \nDelta: {}", iter, delta);
-		if delta < criteria || iter > maxiter {
-			println!("SCF finished!");
-			break
-		}
-	}
+    // compute total energy & expectation values from the C P and F matrices
+    let e_total = e_elec + z_a as f64 * z_b as f64 / r;
 
-	// compute total energy & expectation values from the C P and F matrices
-	let e_total = e_elec + z_a as f64 * z_b as f64 / r;
+    println!("Converged energies:\n");
+    println!("e_total: {}", e_total);
+    println!("e_elec: {}", e_elec);
 
-	println!("Converged energies:\n");
-	println!("e_total: {}", e_total);
-	println!("e_elec: {}", e_elec);
-
-
-	// print mulliken populations
-	println!("Mulliken populations: {:?}", p.dot(&s))
+    // print mulliken populations
+    println!("Mulliken populations: {:?}", p.dot(&s))
 }
-
 
 fn main() {
     let args = Args::parse();
